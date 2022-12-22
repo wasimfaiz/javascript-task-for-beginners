70e2f807129ecb8c26c2f6879f83e7c632ac1b31
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-23 00:01:14 +0530

    Update script.js

--- Task2/script.js
+++ Task2/script.js
@@ -1,6 +1,29 @@
-var but = document.querySelector("button");
-var con = document.querySelector(".container");
+// var but = document.querySelector("button");
+// var con = document.querySelector(".container");
 
-but.addEventListener("click", function(){
-    con.style.backgroundColor="red"
+// but.addEventListener("click", function(){
+//     con.style.backgroundColor="red"
+// })
+
+
+var button = document.querySelector("button");
+var container = document.querySelector(".container");
+
+
+button.addEventListener("click", function(){
+    container.style.backgroundColor = "yellow"
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
