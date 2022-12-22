a20dc05afee2cfd49f1a3b35b06f7d52119f16f1
Wasim Faiz <thewasimfaiz@gmail.com>
2022-12-22 23:55:35 +0530

    Update script.js

--- Task1/script.js
+++ Task1/script.js
@@ -1,6 +1,15 @@
+// var cir = document.querySelector(".circle");
+
+// window.addEventListener("mousemove", function(dets){
+//     cir.style.left= dets.clientX+"px"
+//     cir.style.top= dets.clientY+"px"
+// })
+
+
+
 var cir = document.querySelector(".circle");
 
 window.addEventListener("mousemove", function(dets){
-    cir.style.left= dets.clientX+"px"
-    cir.style.top= dets.clientY+"px"
+   cir.style.left = dets.clientX+"px"
+   cir.style.top = dets.clientY+"px"
 })
